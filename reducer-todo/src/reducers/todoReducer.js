3322d18fad4077ef8ff06c37c22a2955405c664c
Mada Gower <gmgower@gmail.com>
2019-09-20 19:09:00 -0500

    Build reducer & initial state object.

---
+++ reducer-todo/src/reducers/todoReducer.js
@@ -0,0 +1,18 @@
+export const initialState = {
+
+}
+
+export const todoReducer  = (state, action) => {
+console.log(state, action)
+
+const todoArray = [
+{
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+  }
+]
+
+
+    return state;
+}
