575a87f013c9a06e998c814149a8aa387b05086e
Mada Gower <gmgower@gmail.com>
2019-09-20 20:18:36 -0500

    Setup State.

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 
 import './App.css';
 
+// 4 import initialState, todoReducer
 import {initialState, todoReducer} from './reducers/todoReducer'
 
+// Todo Actions
+// 1 Add todos
+// 2 Toggle completed field
+// 3 Clear completed todos
+
+
 function App() {
-  return (
+
+  // 5 Call useReducer that take in reducer & initial state obj
+  const [ state, dispatch ] = useReducer(initialState, todoReducer);
+  console.log("TCL: App -> state", state)
+ 
+return (
     <div className="App">
     <h1>Test</h1>
     </div>
--- reducer-todo/src/reducers/todoReducer.js
+++ reducer-todo/src/reducers/todoReducer.js
@@ -1,17 +1,22 @@
+// 1 Build initial state
 export const initialState = {
 
+    // 3 Build initial state object
+     todoArray: [
+        {
+            item: 'Learn about reducers',
+            completed: false,
+            id: 3892987589
+          }
+        ],
 }
 
+// 2 Build initial reducer 
 export const todoReducer  = (state, action) => {
 console.log(state, action)
 
-const todoArray = [
-{
-    item: 'Learn about reducers',
-    completed: false,
-    id: 3892987589
-  }
-]
+
+
 
 
     return state;
