35960b69bfe0271057043fa138c30fefc758b812
Mada Gower <gmgower@gmail.com>
2019-09-20 22:26:12 -0500

    Add todos.

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -3,7 +3,9 @@ import React, {useReducer} from 'react';
 import './App.css';
 
 // 4 import initialState, todoReducer
-import {initialState, todoReducer} from './reducers/todoReducer'
+import {initialState, todoReducer} from './reducers/todoReducer.js'
+
+import TodoForm from './components/TodoForm.js'
 
 // Todo Actions
 // 1 Add todos
@@ -12,14 +14,19 @@ import {initialState, todoReducer} from './reducers/todoReducer'
 
 
 function App() {
-
   // 5 Call useReducer that take in reducer & initial state obj
-  const [ state, dispatch ] = useReducer(initialState, todoReducer);
+  const [ state, dispatch ] = useReducer(todoReducer, initialState );
+  console.log("TCL: App -> dispatch", dispatch)
   console.log("TCL: App -> state", state)
- 
+
+  const addTodo = item => {
+    dispatch({type: "ADD_TODO", payload: item})
+  }
+
 return (
     <div className="App">
-    <h1>Test</h1>
+    <h1>Todos</h1>
+    <TodoForm addTodo={addTodo} />
     </div>
   );
 }
--- reducer-todo/src/reducers/todoReducer.js
+++ reducer-todo/src/reducers/todoReducer.js
@@ -1,23 +1,31 @@
 // 1 Build initial state
 export const initialState = {
+  // 3 Build initial state object
+  todoArray: [
+    {
+      item: 'Learn about reducers',
+      completed: false,
+      id: 3892987589
+    }
+  ]
+};
 
-    // 3 Build initial state object
-     todoArray: [
-        {
-            item: 'Learn about reducers',
-            completed: false,
-            id: 3892987589
-          }
-        ],
-}
+// 2 Build initial reducer
+export const todoReducer = (state, action) => {
+  console.log(state, action);
+  switch (action.type) {
+    case 'ADD_TODO':
+      const newTodo = {
+        item: action.payload,
+        completed: false,
+        id: Date.now()
+      };
+      return {
+        ...state,
+        todoArray: [...state.todoArray, newTodo] // state.todoArray.concat(newTodo)
+      };
 
-// 2 Build initial reducer 
-export const todoReducer  = (state, action) => {
-console.log(state, action)
-
-
-
-
-
-    return state;
-}
+    default:
+      return state;
+  }
+};
